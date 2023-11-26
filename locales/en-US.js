3d7daee2121b4cecae96346cda22fb01a1d6211c
shadyaabed <shady@moltaqa.net>
2023-11-26 10:08:14 +0200

    first commit

---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Rider',
+      title: 'A passenger delivery application that aims to provide a competitive service',
+      desc: 'The "Rider" application allows users to access passenger delivery services, requests and transportation, in addition to the services of delegates such as the mall representative and Al Fazaa representative. Users can access the app via popular app stores like Apple Store and Google Play.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Ease of use",
+      desc: 'Rider features a simple and intuitive user interface, allowing you to book your trip easily and quickly.',
+    },
+    {
+      image: '2.svg',
+      title: "Flexibility in options",
+      desc: "You can choose the type of car that suits your needs, whether it is an economical car, a family vehicle, or a luxury car.",
+    },
+    {
+      image: '3.svg',
+      title: "Cost estimation",
+      desc: 'You can find out the expected cost of a trip before you book it, allowing you to plan your budget in advance.'
+    },
+  ],
+  about: {
+    mainTitle: 'Rider app is your ideal choice',
+    title: 'About Rider',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the Rider app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the Rider app now by visiting the App Store on your smartphone, searching for Rider, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
